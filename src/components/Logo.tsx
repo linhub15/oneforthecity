a92869398eb898c4042cc340ea5a9c070598c215
Hubert Lin <linhub15@gmail.com>
2024-03-11 16:29:16 -0600

    style: fancify the logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,13 +1,19 @@
 import Image from "next/image";
 import brandImage from "@/images/brand/logo.jpg";
 
-export function Logo(props: React.ComponentPropsWithoutRef<"svg">) {
+export function Logo(props: React.ComponentPropsWithoutRef<"div">) {
   return (
-    <Image
-      className="rounded-xl"
-      src={brandImage}
-      alt="One for the City logo"
-      width="124"
-    />
+    <div {...props}>
+      <div className="absolute h-24 w-32 bg-gray-300 rounded-xl rotate-3 z-0">
+      </div>
+      <div className="absolute h-24 w-32 bg-gray-200 rounded-xl rotate-12 z-0 shadow">
+      </div>
+      <Image
+        className="absolute rounded-xl h-24 object-cover z-10 shadow-lg"
+        src={brandImage}
+        alt="One for the City logo"
+        width="128"
+      />
+    </div>
   );
 }
