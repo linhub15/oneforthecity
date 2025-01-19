02cfec4cdb6bf543f50ffa78711b766bf6db5138
Hubert Lin <linhub15@users.noreply.github.com>
2025-01-19 15:34:03 -0700

    feat(banner): add vol 5 event

--- src/app/(index)/components/Banner.tsx
+++ src/app/(index)/components/Banner.tsx
@@ -3,7 +3,7 @@ export function Banner() {
     <div className="relative isolate flex items-center justify-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5">
       <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
         <p className="text-sm/6 text-gray-900">
-          <strong className="font-semibold">3-Year Anniversary Jam</strong>
+          <strong className="font-semibold">One for the City Volume 5.0</strong>
           <span className="hidden md:inline">
             <svg
               viewBox="0 0 2 2"
@@ -12,14 +12,14 @@ export function Banner() {
             >
               <circle cx="1" cy="1" r="1" />
             </svg>
-            Edmonton, January 18
+            Edmonton, Jun 28
           </span>
         </p>
         <a
-          className="flex-none rounded-full bg-gray-900 px-3.5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
-          href="https://lu.ma/3zm2zx5d"
+          className="flex items-center justify-center gap-1 flex-none rounded-full bg-gray-900 px-3.5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
+          href="https://lu.ma/nv9k3zi8"
         >
-          Tickets <span aria-hidden="true">&rarr;</span>
+          Tickets <span className="block" aria-hidden="true">&rarr;</span>
         </a>
       </div>
     </div>
