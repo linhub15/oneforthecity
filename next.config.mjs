a164d76b36ccc6ab8b5812efd336996d3f399195
Hubert Lin <linhub15@users.noreply.github.com>
2025-02-03 13:58:10 -0700

    chore: use mjs for next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
-/** @type {import('next').NextConfig} */
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   output: "export",
   trailingSlash: true,
   images: { unoptimized: true },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
